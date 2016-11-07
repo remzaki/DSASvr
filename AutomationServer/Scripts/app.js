e8b63fd7dc34406651b4a04d9074df434e43a81f
remzaki <reimark.cogonon@gmail.com>
2016-11-07 20:37:50 +0800

    Migrated the Elements page codes

--- AutomationServer/Scripts/app.js
+++ AutomationServer/Scripts/app.js
@@ -1,4 +1,7 @@
-﻿var validator = $("#loginForm").validate({
+﻿/***********************
+*   Validators
+***********************/
+var login_validator = $("#loginForm").validate({
     rules: {
         email: {
             required: true
@@ -8,6 +11,7 @@
         }
     }
 });
+
 $("#updateForm").validate({
     rules: {
         oldpassword: {
@@ -27,12 +31,253 @@ $("#updateForm").validate({
     }
 });
 
+var validator = $("#elementForm").validate({
+    rules: {
+        ename: {
+            required: true
+        },
+        eval: {
+            required: true
+        },
+        escn: {
+            accept: "image/*"
+        }
+    },
+    messages: {
+        escn: {
+            accept: "File should be an image"
+        }
+    }
+});
+/***********************
+*   Validators
+***********************/
+
+var tokenKey = 'accessToken';
+
+File.prototype.toBase64 = function (callback) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        callback(e.target.result)
+    };
+    reader.onerror = function (e) {
+        callback(null);
+    };
+    reader.readAsDataURL(this);
+};
+
+$("input[type='file'][name='escn']").on("change", function () {
+    var form = $("#elementForm")
+    var selectedFile = this.files[0];
+    if (form.valid() && (typeof selectedFile !== 'undefined') && (selectedFile !== null)) {
+        selectedFile.toBase64(function (base64) {
+            if (base64 !== null) {
+                $("#element-scn").val(base64)
+                $(".modal-body .scn").show();
+                $('.modal-body .scn img').attr('alt', 'New upload').attr('src', base64)
+            }
+            else {
+                console.log("Error in encoding image to base64")
+            }
+        });
+    }
+    else {
+        console.log("Form validity: " + form.valid() + " | Selected file: " + selectedFile)
+    }
+})
+
+var elementModel = function () {
+    var self = this;
+    self.loading = ko.observable(true);
+    self.elements = ko.observableArray();
+    self.error = ko.observable();
+    self.msg = ko.observable();
+    self.search = ko.observable('');
+    self.name = ko.observable();
+    self.value = ko.observable();
+    self.Desc = ko.observable();
+    self.screenshot = ko.observable();
+
+    function ApiRequest(uri, method, headers, data) {
+        self.error('');
+        return $.ajax({
+            type: method,
+            url: uri,
+            headers: headers,
+            dataType: 'json',
+            contentType: 'application/json',
+            data: data ? JSON.stringify(data) : null
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+            self.error(errorThrown);
+        });
+    }
+
+    var ElementsUri = '/api/elements';
+    function getAllElements() {
+        self.loading(true);
+        ApiRequest(ElementsUri, 'GET').done(function (data) {
+            //console.log(data);
+            self.elements(data);
+            self.loading(false);
+        });
+    }
+
+    function gotoAnchorElement() {
+        var hash = location.hash
+        hash = hash.replace('#', '')
+
+        if (hash != '') {
+            self.search(hash);
+        }
+    }
+
+    self.reloadElements = function () {
+        self.loading(true);
+        self.elements('');
+        getAllElements();
+        self.clearSearch();
+    }
+
+    self.submitElement = function (formElement) {
+        headers = {};
+        var token = sessionStorage.getItem(tokenKey);
+        if (token) {
+            headers.Authorization = 'Bearer ' + token;
+        } else {
+            self.error("Are you trying to do something not good?")
+            return;
+        }
+        if ($(formElement).valid()) {
+            self.msg()
+            self.name()
+            var method, msg_str
+            var uri = ElementsUri
+            var name = $("#element-name", formElement).val()
+            name = name.replace(/ /g, '_')
+            name = encodeURI(name)
+            var value = $("#element-val", formElement).val()
+            var Desc = $("#element-desc", formElement).val()
+            var screenshot = $("#element-scn", formElement).val()
+            var data =
+                {
+                    "Name": name,
+                    "Value": value,
+                    "Description": Desc,
+                    "Screenshot": screenshot
+                }
+
+            if (self.act() == "Update") {
+                method = "PUT"
+                uri = uri + "?name=" + name
+                msg_str = "Perfectly updated "
+            } else {
+                method = "POST"
+                msg_str = "Awesomely added "
+            }
+            if (screenshot == "") {
+                delete data.Screenshot;
+            }
+
+            ApiRequest(uri, method, headers, data).done(function (response) {
+                $('#msg').show();
+                $('#elementModal').modal('hide');
+                self.msg(msg_str)
+                self.name(name)
+                $('#msg').delay(6000).fadeOut('slow');
+                getAllElements();
+            });
+        }
+    }
+
+    self.clearSearch = function () {
+        self.search('');
+        location.hash = "";
+    }
+    self.filterElements = ko.computed(function () {
+        $('#results').show();
+        self.error('');
+        var results = ko.utils.arrayFilter(self.elements(), function (element) {
+            return (
+                    (
+                        self.search().length == 0 ||
+                        element.Name.toLowerCase().indexOf(self.search().toLowerCase()) > -1 ||
+                        element.Value.toLowerCase().indexOf(self.search().toLowerCase()) > -1 ||
+                        element.Desc.toLowerCase().indexOf(self.search().toLowerCase()) > -1
+                    )
+            );
+        });
+        if (results.length > 0) {
+            return results;
+        }
+        else {
+            self.error('Poor, oh poor, no results found')
+        }
+    });
+
+    self.anchorElement = function (element) {
+        location.hash = element.Name;
+    }
+
+    self.act = ko.observable();
+    self.mod_loading = ko.observable();
+    self.mod_title = ko.observable('Element');
+    self.mod_btn = ko.observable('Submit');
+
+    self.selectElement = function (act, data) {
+        $('#msg').hide();
+        $('#elementModal .modal-body input[type=file]').val('')
+        validator.resetForm();
+        var request_flag = true;
+        self.mod_loading(true);
+        self.act(act);
+        switch (act) {
+            case 'View':
+                self.mod_title('View Element')
+                self.mod_btn('');
+                break;
+            case 'Update':
+                self.mod_title('Update Element')
+                self.mod_btn('Update');
+                break;
+            case 'Add':
+                self.name('');
+                self.value('');
+                self.Desc('');
+                self.screenshot('');
+                self.mod_loading(false);
+                self.mod_title('New Element')
+                self.mod_btn('Submit');
+                request_flag = false;
+                break;
+        }
+        //if (self.name() != data.Name) {
+            if (request_flag) {
+                uri = ElementsUri
+                uri += '?name=' + data.Name
+                ApiRequest(uri, 'GET').done(function (e) {
+                    self.name(e.Name);
+                    self.value(e.Value);
+                    self.Desc(e.Desc);
+                    self.screenshot(e.Screenshot);
+                    self.mod_loading(false);
+                });
+            }
+        //} else {
+        //    self.mod_loading(false);
+        //}
+    }
+
+    getAllElements();
+
+    gotoAnchorElement();
+}
+
 var viewModel = function () {
     var self = this;
-    var tokenKey = 'accessToken';
     self.user = ko.observable(sessionStorage.getItem('User'));
     self.account_act = ko.observable(null);
     self.login_msg = ko.observable();
+    self.elementModel = new elementModel;
 
     self.Login = function (formElement) {
         if ($(formElement).valid()) {
@@ -59,7 +304,7 @@ var viewModel = function () {
                 self.login_msg(jqXHR.responseJSON.error_description);
             });
 
-            validator.resetForm();
+            login_validator.resetForm();
         }
     }
 
@@ -87,7 +332,7 @@ var viewModel = function () {
                 contentType: 'application/json; charset=utf-8',
                 data: JSON.stringify(accountData)
             }).done(function () {
-                validator.resetForm();
+                login_validator.resetForm();
                 formElement.reset();
                 self.login_msg("Password change successful");
             }).fail(function (jqXHR, textStatus, errorThrown) {
@@ -114,7 +359,7 @@ var viewModel = function () {
     }
 
     self.selectAccount = function (act) {
-        validator.resetForm();
+        login_validator.resetForm();
         self.account_act(act);
     }
     
